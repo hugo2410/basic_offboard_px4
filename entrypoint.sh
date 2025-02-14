936dcb6252fa4344d32f4b771360d2c2a79350d3
Hugo Birch <hugobirch@hotmail.fr>
2025-02-14 13:30:38 +0000

    FIX: source ros2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
+source /opt/ros/humble/setup.bash
+
 # Build the acton workspace
 cd ~/ros_ws && \
     colcon build &&\
     source ~/ros_ws/install/setup.bash
-
-# Source ROS 2 setup
-source /opt/ros/humble/setup.bash
 
 # Source PX4
 cd ~/PX4-Autopilot
@@ -24,12 +23,12 @@ ros2 launch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557" &
 # Wait for MAVROS to start
 sleep 10
 
-ros2 param set /mavros/param  NAV_DLL_ACT 0 &
-ros2 param set /mavros/param  COM_RC_IN_MODE 4 &
-ros2 param set /mavros/param  COM_RCL_EXCEPT 4 &
+ros2 param set /mavros/param  NAV_DLL_ACT 0
+ros2 param set /mavros/param  COM_RC_IN_MODE 4
+ros2 param set /mavros/param  COM_RCL_EXCEPT 4
 
 # Start mission node
-ros2 run mission_control mission_control &
+ros2 run mission_control mission_control_node &
 
 # Keep the container running
 wait

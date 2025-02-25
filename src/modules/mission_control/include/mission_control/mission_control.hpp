aa53aad5bc70ebb6a702f9aa9a13131a6742c39b
Hugo Birch <hugobirch@hotmail.fr>
2025-02-25 10:13:41 +0000

    chore: pre commmit hook fixes

--- src/modules/mission_control/include/mission_control/mission_control.hpp
+++ src/modules/mission_control/include/mission_control/mission_control.hpp
@@ -1,52 +1,55 @@
-#ifndef MISSION_CONTROL_HPP
-#define MISSION_CONTROL_HPP
-
-#include "rclcpp/rclcpp.hpp"
-#include "mavros_msgs/msg/state.hpp"
-#include "mavros_msgs/srv/command_bool.hpp"
-#include "mavros_msgs/srv/set_mode.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
+// Copyright 2025 Hugo Birch
+#ifndef SRC_MODULES_MISSION_CONTROL_INCLUDE_MISSION_CONTROL_MISSION_CONTROL_HPP_
+#define SRC_MODULES_MISSION_CONTROL_INCLUDE_MISSION_CONTROL_MISSION_CONTROL_HPP_
 
 #include <chrono>
 #include <string>
 
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "mavros_msgs/msg/state.hpp"
+#include "mavros_msgs/srv/command_bool.hpp"
+#include "mavros_msgs/srv/set_mode.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "rclcpp/rclcpp.hpp"
+
 class MissionControl : public rclcpp::Node {
-public:
-    MissionControl();
-    void postConstructionSetup();
+ public:
+  MissionControl();
+  void postConstructionSetup();
 
-private:
-    // State variables
-    mavros_msgs::msg::State current_state_;
-    std::string mission_state_;
-    bool armed_;
-    double cruise_altitude_;
-    double external_cmd_timeout_; // seconds
-    geometry_msgs::msg::TwistStamped::SharedPtr external_velocity_command_;
-    geometry_msgs::msg::Pose current_position_;
-    rclcpp::Time last_external_cmd_time_;
+ private:
+  // State variables
+  mavros_msgs::msg::State current_state_;
+  std::string mission_state_;
+  bool armed_;
+  double cruise_altitude_;
+  double external_cmd_timeout_;  // seconds
+  geometry_msgs::msg::TwistStamped::SharedPtr external_velocity_command_;
+  geometry_msgs::msg::Pose current_position_;
+  rclcpp::Time last_external_cmd_time_;
 
-    // ROS interfaces
-    rclcpp::Subscription<mavros_msgs::msg::State>::SharedPtr state_sub_;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr position_sub_;
-    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr velocity_setpoint_sub_;
-    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr vel_pub_;
-    rclcpp::Client<mavros_msgs::srv::SetMode>::SharedPtr set_mode_client_;
-    rclcpp::Client<mavros_msgs::srv::CommandBool>::SharedPtr arm_client_;
-    rclcpp::TimerBase::SharedPtr timer_;
+  // ROS interfaces
+  rclcpp::Subscription<mavros_msgs::msg::State>::SharedPtr state_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr position_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr
+      velocity_setpoint_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr vel_pub_;
+  rclcpp::Client<mavros_msgs::srv::SetMode>::SharedPtr set_mode_client_;
+  rclcpp::Client<mavros_msgs::srv::CommandBool>::SharedPtr arm_client_;
+  rclcpp::TimerBase::SharedPtr timer_;
 
-    // Callback functions
-    void stateCallback(const mavros_msgs::msg::State::SharedPtr msg);
-    void positionCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
-    void velocitySetpointCallback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
+  // Callback functions
+  void stateCallback(const mavros_msgs::msg::State::SharedPtr msg);
+  void positionCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+  void velocitySetpointCallback(
+      const geometry_msgs::msg::TwistStamped::SharedPtr msg);
 
-    // Utility functions
-    bool checkExternalCmdTimeout();
-    void setMode(const std::string &mode);
-    void arm(bool value);
-    void initializeMission();
-    void timerCallback();
+  // Utility functions
+  bool checkExternalCmdTimeout();
+  void setMode(const std::string &mode);
+  void arm(bool value);
+  void initializeMission();
+  void timerCallback();
 };
 
-#endif // MISSION_CONTROL_HPP
+#endif  // SRC_MODULES_MISSION_CONTROL_INCLUDE_MISSION_CONTROL_MISSION_CONTROL_HPP_
